a1d60862df211ee57da31171f5d4cd77030875ea
Garth-Faigley <gfaigley@gmail.com>
2016-05-22 17:54:12 -0500

    First GitHub commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BaseballScoreboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ScoreboardData scoreboardData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            scoreboardData = new ScoreboardData();
+            this.DataContext = scoreboardData;
+        }
+
+        private void IncrementOut(object sender, RoutedEventArgs e)
+        {
+            Button sendingButton = (Button)sender;
+            if (sendingButton.Name == "OutIncrement")
+            {
+                if (scoreboardData.CurrentOut < 3)
+                {
+                    scoreboardData.CurrentOut += 1;
+                }
+                else
+                {
+                    scoreboardData.CurrentOut = 0;
+                }
+            }
+            else
+            {
+                if (scoreboardData.CurrentOut > 0)
+                {
+                    scoreboardData.CurrentOut -= 1;
+                }
+            }
+        }
+
+
+        private void IncrementInning(object sender, RoutedEventArgs e)
+        {
+            Button sendingButton = (Button)sender;
+            if (sendingButton.Name == "InningIncrement")
+            {
+                if (scoreboardData.AtBat == "^")
+                {
+                    scoreboardData.AtBat = "v";
+                }
+                else
+                {
+                    scoreboardData.AtBat = "^";
+                    scoreboardData.CurrentInning += 1;
+                }
+            }
+            else
+            {
+                //if (scoreboardData.CurrentInning > 1 && scoreboardData.AtBat != "^")
+                //{
+                if (scoreboardData.AtBat == "^")
+                {
+                    if (scoreboardData.CurrentInning > 1)
+                    {
+                        scoreboardData.AtBat = "v";
+                        scoreboardData.CurrentInning -= 1;
+                    }
+                }
+                else
+                {
+                    scoreboardData.AtBat = "^";
+                }
+                //}
+            }
+        }
+
+        private void IncrementHome(object sender, RoutedEventArgs e)
+        {
+            Button sendingButton = (Button)sender;
+            if (sendingButton.Name == "HomeIncrement")
+            {
+                scoreboardData.HomeScore += 1;
+            }
+            else
+            {
+                if (scoreboardData.HomeScore > 0)
+                {
+                    scoreboardData.HomeScore -= 1;
+                }
+            }
+        }
+
+        private void IncrementAway(object sender, RoutedEventArgs e)
+        {
+            Button sendingButton = (Button)sender;
+            if (sendingButton.Name == "AwayIncrement")
+            {
+                scoreboardData.AwayScore += 1;
+            }
+            else
+            {
+                if (scoreboardData.AwayScore > 0)
+                {
+                    scoreboardData.AwayScore -= 1;
+                }
+            }
+        }
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.I)
+            {
+                if (scoreboardData.AtBat == "^")
+                {
+                    scoreboardData.AtBat = "v";
+                }
+                else
+                {
+                    scoreboardData.AtBat = "^";
+                    scoreboardData.CurrentInning += 1;
+                }
+            }
+            if (e.Key == Key.A)
+            {
+                scoreboardData.AwayScore += 1;
+            }
+            if (e.Key == Key.H)
+            {
+                scoreboardData.HomeScore += 1;
+            }
+            if (e.Key == Key.O)
+            {
+                if (scoreboardData.CurrentOut < 3)
+                {
+                    scoreboardData.CurrentOut += 1;
+                }
+                else
+                {
+                    scoreboardData.CurrentOut = 0;
+                }
+            }
+        }
+    }
+}
---
+++ ScoreboardData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballScoreboard
+{
+    class ScoreboardData : INotifyPropertyChanged
+    {
+
+        private int homeScore;
+        private int awayScore;
+        private int currentInning;
+        private int currentOut;
+        private string atBat;
+
+        public int HomeScore
+        {
+            get { return homeScore; }
+            set { homeScore = value; OnPropertyChanged("HomeScore"); }
+        }
+
+        public int AwayScore
+        {
+            get { return awayScore; }
+            set { awayScore = value; OnPropertyChanged("AwayScore"); }
+        }
+
+        public int CurrentInning
+        {
+            get { return currentInning; }
+            set { currentInning = value; OnPropertyChanged("CurrentInning"); }
+        }
+
+        public int CurrentOut
+        {
+            get { return currentOut; }
+            set { currentOut = value; OnPropertyChanged("CurrentOut"); }
+
+        }
+
+        public string AtBat
+        {
+            get { return atBat; }
+            set { atBat = value; OnPropertyChanged("AtBat"); }
+        }
+        
+        public ScoreboardData()
+        {
+            HomeScore = 0;
+            AwayScore = 0;
+            CurrentInning = 1;
+            CurrentOut = 0;
+            AtBat = "^";
+        }
+
+
+        // boilerplate code supporting PropertyChanged events
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+    }
+}
